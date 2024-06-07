9e74ce19c94eb3b2246a14f5b110e0f1e810de1e
Ghita <18383@ecam.be>
2024-06-08 00:46:33 +0200

    blank Home page functional, installed babel-preset-expo, updated dependencies, updated .gitignore

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: ["babel-preset-expo"],
-  };
+  const presets = ['babel-preset-expo'];
+    
+      return {
+        presets,
+      };
 };
