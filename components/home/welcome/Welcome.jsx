1ee0f56a62dbf9d96d96464973df1a13273ac2f0
Ghita <18383@ecam.be>
2024-06-08 03:23:27 +0200

    finish setup of HomePage, show list of cards,aesthetic etc

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,27 @@
-import {React, useState} from 'react'
+import React,  {useState} from 'react'
 import { View, Text, TextInput, TouchableOpacity, Image, FlatList  } from 'react-native'
 import { useRouter } from 'expo-router';
 
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants';
 
+const jobTypes = ["Full-time", "Part-time", "Interim" ];
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-time');
+
+  const renderJobType = ({ item }) => (
+    <TouchableOpacity 
+      style={styles.tab(activeJobType, item)} 
+      onPress={() => {
+        setActiveJobType(item);
+        router.push('/search/${item}')
+      }}
+    >
+      <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+    </TouchableOpacity>
+  );
 
   return (
     <View>
@@ -17,10 +32,33 @@ const Welcome = () => {
 
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput}/>
+          <TextInput 
+          style={styles.searchInput}
+          value=""
+          onChange={() => {}}
+          placeholder='What are you looking for?'
+          />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+            source={icons.search}
+            resizeMode= "contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
       </View>
       
+      <View style={styles.tabsContainer}>
+        <FlatList 
+          data={jobTypes}
+          renderItem={renderJobType}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+        />
+      </View>
+
     </View>
   )
 }
